012b7418df71369de0ca442620367fdbf70ddd88
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-03-16 15:36:31 +0000

    Installed sanitizer module

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const fs = require('fs');
 const express = require('express');
 
-var app = require('express')();
-var http = require('http').Server(app);
-var bodyParser = require('body-parser');
+let app = require('express')();
+let http = require('http').Server(app);
+let bodyParser = require('body-parser');
+let sanitizer = require('sanitizer');
 
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const Database = require("@replit/database");
+const db = new Database();
+
 app.get('', function (req, res) {
   const index = __dirname + '/public/static/index.html';
 
   res.sendFile(index);
+});
+
+app.post('/stat', function (req, res) {
+  const team_num = req.body.team;
+  const match = req.body.match;
+  const info = req.body.info;
+});
+
+let chatbox = "";
+
+app.post('/chat', function (req, res) {
+  const chat_user = req.body.user;
+  const msg = req.body.msg;
+
+  const clean_user = sanitizer.escape(chat_user);
+  const clean_msg = sanitizer.escape(msg);
+});
+
+app.get('/chat', function (req, res) {
+  res.send(chatbox);
 });
 
 http.listen(port, function(){
