a31ba65e2f0bde8440a6f940d7acf94a42285ac7
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-03-16 13:00:44 +0000

    COMPLETE REDESIGN!

---
+++ public/scripts/component.js
@@ -0,0 +1,7 @@
+const teamBtn = document.getElementById("team");
+const statBtn = document.getElementById("stat");
+const comsBtn = document.getElementById("coms");
+
+teamBtn.onclick = function () {
+  
+}
