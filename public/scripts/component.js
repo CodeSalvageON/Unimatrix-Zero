a6824f624c759b83be3b1d059f7abe4cccd3a378
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-03-16 13:12:43 +0000

    Completed Team form

--- public/scripts/component.js
+++ public/scripts/component.js
@@ -2,6 +2,24 @@ const teamBtn = document.getElementById("team");
 const statBtn = document.getElementById("stat");
 const comsBtn = document.getElementById("coms");
 
+const teamSec = document.getElementById("teamSec");
+const statSec = document.getElementById("statSec");
+const comsSec = document.getElementById("comsSec");
+
+teamSec.style.display = "none";
+statSec.style.display = "none";
+comsSec.style.display = "none";
+
+const teamForm = document.getElementById("team-form");
+const teamNumber = document.getElementById("team-number");
+
 teamBtn.onclick = function () {
-  
+  teamSec.style.display = "block";
+  statSec.style.display = "none";
+  comsSec.style.display = "none";
+}
+
+teamForm.onsubmit = function () {
+  event.preventDefault();
+     window.open("https://www.thebluealliance.com/team/" + teamNumber.value)
 }
